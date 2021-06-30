3afc5d56eb5ec63b39877bbdb6208701412c1d2b
Regis A. Ely <regisaely@gmail.com>
2021-06-30 18:32:24 -0300

    Decode bov_gui

--- bov_gui.py
+++ bov_gui.py
@@ -3,4 +3,4 @@ import subprocess
 
 code = enterbox("Code:")
 quote = subprocess.check_output(['./getquote.sh', code])
-msgbox(quote.split()[0] + ': ' + '{0:.2f}'.format(float(quote.split()[1])))
+msgbox(quote.split()[0].decode("utf-8") + ': ' + '{0:.2f}'.format(float(quote.split()[1])))
